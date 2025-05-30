637a73b5bb555e1ace5698970427f38b64b58656
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-05-29 23:44:08 -0400

    making interview cards

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -22,14 +23,19 @@ const page = () => {
         <section className='flex flex-col gap-6'>
           <h2> Your interview </h2>
           <div className='interviews-section'>
-            <p> You haven&apos;t taken any interviews yet </p>
+            {dummyInterviews.map((interview) => (
+              <InterviewCard {...interview}/>
+            ))}
           </div>
         </section>
 
         <section className='flex flex-col gap-6 mt-8'>
           <h2> Take an Interview</h2>
           <div className='interviews-section'>
-            <p> There are no interviews available</p>
+            {dummyInterviews.map((interview) => (
+              <InterviewCard {...interview}/>
+            ))}
+            <p> YOu havent taken any interview yet</p>
           </div>
         </section>
       </>
---
+++ components/InterviewCard.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InterviewCard = ({interviewId, userId, role, type, techstack, createdAt }: InterviewCardProps) => {
+    const feedback = null as Feedback | null;
+    const normalizedType = /mix/gi.test(type) ? 'Mixed': type ? 'Mixed' : type;
+
+
+    return (
+        <div>
+        InterviewCard
+        </div>
+    )
+}
+
+export default InterviewCard
