7e3254fd44d265e2455550aec0888320be8e1e4b
Jacobli6177 <101788026+Jacobli6177@users.noreply.github.com>
2025-06-02 22:32:28 -0400

    added authentication to every page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
 import React, { ReactNode } from 'react'
+import { redirect } from 'next/navigation';
+import { isAuthenticated } from '@/lib/actions/auth..action';
 
-const AuthLayout = ( { children} : {children: ReactNode }) => {
+const AuthLayout = async( { children} : {children: ReactNode }) => {
+    const isUserAuthenicated = await isAuthenticated();
+  
+    if(!isUserAuthenicated) redirect('/'); 
   return (
     <div className="auth-layout">
       {children}
